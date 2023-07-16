d9feafc8554ff49ddb4ed4848157369180a9c0e6
oturikumwe <oturikumwe@gmail.com>
2023-07-16 10:23:06 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,21 @@
-#include <stdio.h>
 /**
- *Hllo c world
- *function returning zero
+ *File: 4-puts.c
+ *Auth: Brennan D Baraban <375@holbertonschool.com>
  */
+/**
+ *Programming is like building a multilingual
+ *puzzle, followed by a new line.
+ *Return: Always 0.
+ */
+#include <stdio.h>
 int main(void)
 {
+	/**
+	 * Puts desplays the following stated statment.
+	 */
 	puts("\"Programming is like building a multilingual puzzle");
- 	return (0);
+	/**
+	 *The return function returns 0.
+	 */
+	return (0);
 }
