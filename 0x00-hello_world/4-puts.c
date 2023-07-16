cd5409c96f023f0210f7d54720eda1c826f05571
oturikumwe <oturikumwe@gmail.com>
2023-07-16 10:31:10 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,16 @@
-/**
- *File: 4-puts.c
- *Auth: Brennan D Baraban <375@holbertonschool.com>
- */
-/**
- *Programming is like building a multilingual
- *puzzle, followed by a new line.
- *Return: Always 0.
+/*
+ * File: 4-puts.c
+ * Auth: Brennan D Baraban <375@holbertonschool.com>
  */
 #include <stdio.h>
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	/**
-	 * Puts desplays the following stated statment.
-	 */
 	puts("\"Programming is like building a multilingual puzzle");
-	/**
-	 *The return function returns 0.
-	 */
 	return (0);
 }
