e91a70c0e10c1317352aaff26a67b3ae9159e8f2
oturikumwe <oturikumwe@gmail.com>
2023-07-29 21:37:53 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,22 @@
-#include <main.h>
+#include "main.h"
+
 /**
- * main - function to print
- *Discriptions to print _putchar
- * Return: 0
- */
+* main - print _putchar
+* is written by mbah
+* Return: 0
+*/
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
+
+
